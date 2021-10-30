3a0e0d567761f1834f0ed0c30233db16e21dce1a
kamal heydari <kamal3heydari@gmail.com>
2021-10-30 15:04:11 +0330

    drawing panel added

--- src/components/DrawingPanel.js
+++ src/components/DrawingPanel.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const DrawingPanel = () => {
-  return <div>drawing component</div>;
+//? Style
+import "../styles/drawingPanel.scss";
+
+//? Components
+import Row from "./Row";
+
+const DrawingPanel = ({ width, height, selectedColor }) => {
+  let rows = [];
+
+  for (let i = 0; i < width; i++) {
+    rows.push(<Row key={i} />);
+  }
+
+  return (
+    <div id="drawingPanel">
+      <div id="pixels">{rows}</div>
+    </div>
+  );
 };
 
 export default DrawingPanel;
